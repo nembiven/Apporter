264f0273da0a28080949a9fd37d453107dfcd1ba
Nicole Saez <nicole.saezt@utem.cl>
2022-12-01 22:13:48 -0300

    [ADD] Se agrega el mapa

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
+
 export interface MapInterface {}
 
 const Map : React.FC<MapInterface> = () => {
 	return <>
-	<Text>Mapa</Text>
+	<Text>Mapa</Text><View style={styles.container}>
+      <MapView style={styles.map} 
+	  initialRegion={{
+      latitude: -33.4555447,
+      longitude: -70.642035,
+      latitudeDelta: 0.05,
+      longitudeDelta: 0.05,
+    }} />
+    </View>
 	</>
 };
-
+const styles = StyleSheet.create({
+	container: {
+	  flex: 1,
+	},
+	map: {
+	  width: '100%',
+	  height: '100%',
+	},
+  });
 export default Map;
