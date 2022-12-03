f34deb1e029402084419adf61472f2cb26d2c639
Nicole Saez <nicole.saezt@utem.cl>
2022-12-03 09:58:41 -0300

    [modify]map

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
+
 
 export interface MapInterface {}
 
 const Map : React.FC<MapInterface> = () => {
 	return <>
-	<Text>Mapa</Text><View style={styles.container}>
+	  <View style={styles.container}>
       <MapView style={styles.map} 
 	  initialRegion={{
       latitude: -33.4555447,
       longitude: -70.642035,
-      latitudeDelta: 0.05,
-      longitudeDelta: 0.05,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
     }} />
     </View>
 	</>
