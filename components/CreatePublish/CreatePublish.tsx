ee563937fed6d42e652e4d069592ff6a796bb5db
Diego <diego.tapialeal@utem.cl>
2022-12-16 06:29:59 -0300

    Front: Crear Publicacion

--- components/CreatePublish/CreatePublish.tsx
+++ components/CreatePublish/CreatePublish.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, SafeAreaView, View, Pressable,TextInput, Button, Alert, StyleSheet, ScrollView  } from 'react-native';
+import { TouchableOpacity, Text, SafeAreaView, View, Pressable,TextInput, Button, Alert, StyleSheet, ScrollView  } from 'react-native';
 import { useEffect, useState } from 'react';
 import {addDoc,getDocs,collection, getFirestore } from 'firebase/firestore';
 import {Publish} from '../../models';
@@ -30,100 +30,130 @@ const CreatePublish : React.FC<CreatePublishInterface> = () => {
         }
     }
 	return <>
-	<SafeAreaView>
-	<ScrollView><View style={styles.title}>
-      <Text>Título:</Text>
+	<SafeAreaView style={styles.containerMain}>
+	<ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Título:</Text>
       <TextInput
-        placeholder=' Enter titulo'
-		style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
+        placeholder=''
+		    style={styles.label}
         onChangeText = {(text) => setTitle(text)}
-	  />
-	  
-	  <Text>Descripción:</Text>
+	    />
+      <Text style={styles.title}>Descripción:</Text>
       <TextInput
-		placeholder=' Enter descripción'
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
+        placeholder=''
+        style={styles.labelDes}
+        allowFontScaling
+        multiline={true}
         onChangeText = {(text) => setdescription(text)}
       />
-	  	  <Text>Latitud:</Text>
+	  	<Text style={styles.title}>Latitud:</Text>
       <TextInput
-		placeholder=' Enter latitud'
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
+		    placeholder=''
+        style={styles.label}
         onChangeText = {(text) => setlat(text)}
       />
-	  	  <Text>Longitud:</Text>
+	  	<Text style={styles.title}>Longitud:</Text>
       <TextInput
-		placeholder=' Enter longitud'
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
+		    placeholder=''
+        style={styles.label}
         onChangeText = {(text) => setlong(text)}
       />
-		<Text>Etiqueta:</Text>
+		  <Text style={styles.title}>Etiqueta:</Text>
       <TextInput
-	  placeholder=' Enter etiqueta'
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
+	      placeholder=''
+        style={styles.label}
         onChangeText = {(text) => setlabel(text)}
       />
-
+      <View style={styles.cont}>
+        <TouchableOpacity
+          onPress={addPublish}
+          style={styles.button}
+          activeOpacity={1}>
+          <Text style={styles.textButton}>Publicar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-
-	<View style={styles.fixToText}>
-
-	<Button 
-          title="Publicar"
-          onPress={() => addPublish()}
-        />
-
-	</View>
 	</ScrollView>
 	</SafeAreaView>
 	</>
 };
 
 const styles = StyleSheet.create({
-	input: {
-	  height: 40,
-	  margin: 12,
-	  borderWidth: 1,
-	  padding: 10,
-	},
-	container: {
-	  flex: 1,
-	  justifyContent: 'center',
-	  marginHorizontal: 16,
-	},
-	title: {
-	  textAlign: 'center',
+  containerMain:{
+    flex: 1,
+    backgroundColor: '#F3AE5F',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+	container:{
 	  marginVertical: 20,
 	  marginHorizontal: 10,
+    backgroundColor: '#FFF3EB',
+    borderRadius: 10,
 	},
-	fixToText: {
-	  flexDirection: 'row',
-	  justifyContent: 'space-between',
+  cont:{
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+	title: {
+	  color: '#333333',
+		backgroundColor: 'transparent',
+		fontSize: 20,
+		textAlign: 'left',
+    marginTop: 10,
+    marginStart: 10,
 	},
-	separator: {
-	  marginVertical: 8,
-	  borderBottomColor: '#737373',
-	  borderBottomWidth: StyleSheet.hairlineWidth,
+  label:{ //Label de input
+    // alignContent: 'center',
+    // justifyContent: 'center',
+    backgroundColor: '#FFF3EB',
+    position: 'relative',
+    width: 340,
+    height: 40,
+    borderColor: '#0B87BA',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 10,
+    color: '#333333',
+    fontSize: 15,
+    textAlign: 'left',
+    margin: 5,
+    padding: 10,
+  },
+  labelDes:{ //Label de input
+    // alignContent: 'center',
+    // justifyContent: 'center',
+    backgroundColor: '#FFF3EB',
+    position: 'relative',
+    width: 340,
+    height: 90,
+    borderColor: '#0B87BA',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 10,
+    color: '#333333',
+    fontSize: 15,
+    textAlign: 'left',
+    margin: 5,
+    textAlignVertical: 'top',
+    padding: 10,
+  },
+  textButton:{
+		color: '#FFF3EB',
+		backgroundColor: 'transparent',
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	button: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#0B87BA',
+		height: 50,
+		width: 200,
+		borderRadius: 15,
+		margin: 10,
 	},
   });
 export default CreatePublish;
--- pages/Details/Details.tsx
+++ pages/Details/Details.tsx
@@ -1,17 +1,50 @@
 import React from 'react';
-import { ActivityIndicator, Button, Pressable, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, ActivityIndicator, Button, Pressable, Text, View, Image, TouchableOpacity } from 'react-native';
 export interface DetailsInterface {}
 
 const Details : React.FC<DetailsInterface> = (props) => {
 	const info = props.route.params
 	return <>
-	
-		<Text>
-			Titulo: {info.title} {"\n"}
-			Id : {info.id}~{"\n"}
-			{info.isActive}
-		</Text>
+	<View style={styles.container}>
+		<View style={styles.containerInfo}>
+			<Text style={styles.title}>{info.title}</Text>
+			<Text style={styles.description}> Id : {info.id}</Text>
+			<Text style={styles.description}> isActive: {info.isActive}</Text>
+			<Text style={styles.description}> Descripcion: {info.description}</Text>
+			<Text style={styles.description}> Usuario: {info.username}</Text>
+		</View>
+	</View>
 	</>;
 };
+
+const styles = StyleSheet.create({
+	container:{
+		flex: 1,
+		backgroundColor:'#F3AE5F',
+		alignItems : 'center',
+		justifyContent : 'center',
+		padding: 40,
+	},
+	containerInfo:{
+		flex: 1,
+		backgroundColor:'#FFF3EB',
+		borderRadius: 10,
+		width: '100%',
+	},
+	title:{
+		textAlign: 'center',
+		fontSize: 20,
+		color: "#333333",
+		margin: 10,
+	},
+	description:{
+		textAlign: 'justify',
+		marginLeft: 10,
+		marginRight: 10,
+		lineHeight: 20,
+	  	fontSize: 15,
+		color: "#4d4d4d",
+	},
+});
 
 export default Details;
